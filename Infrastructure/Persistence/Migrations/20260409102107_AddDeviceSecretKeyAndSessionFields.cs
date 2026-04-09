742fceb2d43226ec88c4b0b7ed1975d5af675165
Xurshid <khurshid.khujamatov@mail.ru>
2026-04-09 15:29:57 +0500

    Add secret key to DeviceEntity and update migration
    - Added a connection string for PostgreSQL in `Configuration.Production.json`.
    - Introduced a new `SecretKey` property in `DeviceEntity` with a max length of 64 characters.
    - Created migration `AddDeviceSecretKeyAndSessionFields` to add `secret_key` column to the `devices` table.
    - Updated `Designer.cs` to reflect the new property and its constraints.

---
+++ Infrastructure/Persistence/Migrations/20260409102107_AddDeviceSecretKeyAndSessionFields.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeviceSecretKeyAndSessionFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "secret_key",
+                schema: "app",
+                table: "devices",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "secret_key",
+                schema: "app",
+                table: "devices");
+        }
+    }
+}
